3844b2b664b816630f77991befc07d757e98e820
wb7898 <wb7898@arcsoft.com.cn>
2023-04-13 11:17:00 +0800

    1

---
+++ color_text.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "\e[1;30m This is a black text! \e[0m"
+echo -e "\e[1;31m This is a red text! \e[0m"
+echo -e "\e[1;32m This is a green text! \e[0m"
+echo -e "\e[1;33m This is a yellow text! \e[0m"
+echo -e "\e[1;34m This is a blue text! \e[0m"
+echo -e "\e[1;35m This is a magenta text! \e[0m"
+echo -e "\e[1;36m This is a ching text! \e[0m"
+echo -e "\e[1;37m This is a white text! \e[0m"
